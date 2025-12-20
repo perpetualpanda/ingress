f4c3e100c1277968dadcfd843a8384c3b97cd126
PerpetualPanda <97649174+PerpetualPanda@users.noreply.github.com>
2025-12-20 11:52:53 -0800

    fix: missing param

--- deploy.sh
+++ deploy.sh
@@ -3,25 +3,29 @@
 set -euo pipefail
 
 # Parameters
-RESOURCE_GROUP_NAME="${1:?Missing ResourceGroupName}"
-LOCATION="${2:?Missing Location}"
-PRIMARY_DNS_ZONE="${3:?Missing PrimaryDNSZone}"
+BICEP_TEMPLATE="./dns.bicep"
+RESOURCE_GROUP_NAME="${1:?Missing RESOURCE_GROUP_NAME}"
+LOCATION="${2:?Missing LOCATION}"
+PRIMARY_DNS_ZONE="${3:?Missing PRIMARY_DNS_ZONE}"
+EVENT_TRIGGER="${4:-push}" # default to 'push' if not provided
+
+echo "Event Trigger: ${EVENT_TRIGGER}"
 
 # Create resource group
 az group create \
-  --name "$RESOURCE_GROUP_NAME" \
-  --location "$LOCATION" > /dev/null
+  --name "${RESOURCE_GROUP_NAME}" \
+  --location "${LOCATION}" > /dev/null
 
-if [[ "$BUILD_REASON" == "pull_request" ]]; then
+if [[ "${EVENT_TRIGGER}" == "pull_request" ]]; then
     echo "Running WHAT-IF deployment"
     az deployment group what-if \
-      --resource-group "$RESOURCE_GROUP_NAME" \
-      --template-file ./dns.bicep \
-      --parameters PrimaryDNSZone="$PRIMARY_DNS_ZONE"
+      --resource-group "${RESOURCE_GROUP_NAME}" \
+      --template-file ${BICEP_TEMPLATE} \
+      --parameters PrimaryDNSZone="${PRIMARY_DNS_ZONE}"
 else
     echo "Running deployment"
     az deployment group create \
-      --resource-group "$RESOURCE_GROUP_NAME" \
-      --template-file ./dns.bicep \
-      --parameters dnszones_primary_name="$PRIMARY_DNS_ZONE"
+      --resource-group "${RESOURCE_GROUP_NAME}" \
+      --template-file "${BICEP_TEMPLATE}" \
+      --parameters dnszones_primary_name="${PRIMARY_DNS_ZONE}"
 fi
