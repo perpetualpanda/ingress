9497e17385ef9c09571bd1f58ffe2e319ae09f1f
PerpetualPanda <97649174+PerpetualPanda@users.noreply.github.com>
2025-12-20 12:03:42 -0800

    feat: determine deployment type in workflow yaml

--- deploy.sh
+++ deploy.sh
@@ -7,24 +7,24 @@ BICEP_TEMPLATE="./dns.bicep"
 RESOURCE_GROUP_NAME="${1:?Missing RESOURCE_GROUP_NAME}"
 LOCATION="${2:?Missing LOCATION}"
 PRIMARY_DNS_ZONE="${3:?Missing PRIMARY_DNS_ZONE}"
-EVENT_TRIGGER="${4:-push}" # default to 'push' if not provided
+DEPLOYMENT_TYPE="${4:-WHATIF}" # default to 'WHATIF' if not provided
 
-echo "Event Trigger: ${EVENT_TRIGGER}"
+echo "Deployment type: ${DEPLOYMENT_TYPE}"
 
 # Create resource group
 az group create \
   --name "${RESOURCE_GROUP_NAME}" \
   --location "${LOCATION}" > /dev/null
 
-if [[ "${EVENT_TRIGGER}" == "pull_request" ]]; then
-    echo "Running WHAT-IF deployment"
-    az deployment group what-if \
-      --resource-group "${RESOURCE_GROUP_NAME}" \
-      --template-file ${BICEP_TEMPLATE} \
-      --parameters PrimaryDNSZone="${PRIMARY_DNS_ZONE}"
-else
-    echo "Running deployment"
+if [[ "${DEPLOYMENT_TYPE}" == "DEPLOY" ]]; then
+    echo "Running DNS deployment"
     az deployment group create \
+      --resource-group "${RESOURCE_GROUP_NAME}" \
+      --template-file "${BICEP_TEMPLATE}" \
+      --parameters dnszones_primary_name="${PRIMARY_DNS_ZONE}"
+else
+    echo "Running what if DNS deployment"
+    az deployment group what-if \
       --resource-group "${RESOURCE_GROUP_NAME}" \
       --template-file "${BICEP_TEMPLATE}" \
       --parameters dnszones_primary_name="${PRIMARY_DNS_ZONE}"
